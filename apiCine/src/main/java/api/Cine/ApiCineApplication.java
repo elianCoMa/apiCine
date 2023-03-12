8c2306dfebfc1297c6999b566e99595d3613bb8d
elian <elian@LAPTOP-V79H8RNG>
2023-03-12 12:29:16 -0500

    CREACION

---
+++ apiCine/src/main/java/api/Cine/ApiCineApplication.java
@@ -0,0 +1,13 @@
+package api.Cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCineApplication.class, args);
+	}
+
+}
